6c3448fcef43685094e53996798e52f6981c4d25
luizjacomn <luizjacomn@gmail.com>
2016-04-15 18:38:39 -0300

    Declaração de variáveis

---
+++ src/_1_1_nomeacao/Teste_1_1.java
@@ -0,0 +1,22 @@
+package _1_1_nomeacao;
+
+public class Teste_1_1 {
+//	declarações válidas
+	int _v;
+	int $v2;
+	int __v3;
+	int _$;
+	int v_4;
+	
+//	declarações inválidas
+//	int :i1;
+//	int -i2;
+//	int #i3;
+//	int .i4;
+//	int 5;
+	
+//	verbo-substantivo	
+	public int get_v() {
+		return 0;
+	}
+}
--- src/_1_2_declaracao_sources/Teste_1_2.java
+++ src/_1_2_declaracao_sources/Teste_1_2.java
@@ -2,14 +2,27 @@
 package _1_2_declaracao_sources;
 
 //comentário entre a declaração package e import
+/*
+ * A declaração import deve ficar entre a declaração
+ * do package e a declaração da classe.
+ * */
 import java.util.Date;
 
 /*package _1_2_declaracao_sources;
  * a delaração package deve vir antes de qualquer coisa, exceto comentários
 */
+
 //comentário entre a declaração import e a declaração da classe
 
-/* O nome da classe pública deve ser o mesmo nome do arquivo fonte */
+/* O nome da classe pública deve ser o mesmo nome do arquivo fonte 
+ * Erro de compilação
+ * */
+
+//class Teste_1_2 {}
+
+/*
+ * Arquivos com classes não-públicas podem ter qualquer nome
+ */
 public class Teste_1_2 {
 	/**
 	 * Comentário javadoc dentro da classe 
@@ -17,6 +30,7 @@ public class Teste_1_2 {
 	 */
 	public static void main(String[] args) {
 		Livro.main(args);
+		Livro2.main(args);
 	}
 }
 
@@ -29,6 +43,15 @@ class Livro {
 		System.out.println("Teste");
 		System.out.println(new Date());
 		
+	}
+}
+
+/*
+ * Um arquivo pode ter várias classes não-públicas.
+ */
+class Livro2 {
+	public static void main(String[] args) {
+		System.out.println("Teste2");
 	}
 }
 
